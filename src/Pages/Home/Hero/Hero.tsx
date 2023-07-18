6ca4968485c605e3d602b144e8c223af7e4ce158
aichurokdev <aichurokkuttubekovna1@gmail.com>
2023-07-18 16:28:33 +0300

    begin hero

--- src/Pages/Home/Hero/Hero.tsx
+++ src/Pages/Home/Hero/Hero.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import './hero.scss'
+// @ts-ignore
+import bg from "../../../img/Image.png"
 
 const Hero = () => {
     return (
-        <div>
+        <div id='Hero' style={{
+            background: `url("${bg}")no-repeat center/cover`
+        }}>
+            <div className="container">
+                <div className="Hero">
+
+                </div>
+            </div>
             
         </div>
     );
