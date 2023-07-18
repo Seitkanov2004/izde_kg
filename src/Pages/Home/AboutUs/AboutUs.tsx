4a7524bb63ec6c336aa12fd0455bca436b6429cc
Seitkanov2004 <114854470+Seitkanov2004@users.noreply.github.com>
2023-07-18 18:03:57 +0600

    Revert "start-file (#1)" (#2)
    This reverts commit 99f23005c9d8a092ef8f5d042937736e87d7ae39.

--- src/Pages/Home/AboutUs/AboutUs.tsx
+++ src/Pages/Home/AboutUs/AboutUs.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const HomeSearch = () => {
+const AboutUs = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const HomeSearch = () => {
     );
 };
 
-export default HomeSearch;
+export default AboutUs;
