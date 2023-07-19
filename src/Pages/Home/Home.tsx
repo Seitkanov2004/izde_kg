3e6bb62cb21906950448bda9885967af2e44ef14
macbook_pro <syrgabek0131@gmail.com>
2023-07-19 11:32:03 +0600

    I opened home search file

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Hero from "./Hero/Hero";
-
+import HomeSearch from "./HomeSearch/HomeSearch";
 
 const Home = () => {
     return (
         <>
             <Hero/>
+            <HomeSearch/>
         </>
     );
 };
