39463dee128380a449c2de783ccadc45efa2f307
aichurokdev <131878279+aichurokdev@users.noreply.github.com>
2023-08-13 16:38:20 +0600

    new properties detail adaptation (#57)

--- src/Pages/DetailsPage/Location/Location.tsx
+++ src/Pages/DetailsPage/Location/Location.tsx
@@ -9,9 +9,9 @@ const Location = () => {
         <div id='Location'>
             <div className="container">
                 <div className="Location">
-                    <h1>Location</h1>
                     <div className="Location--locationBlock">
                         <div className="map">
+                        <h1>Location</h1>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d889.5621959927396!2d74.58629821702957!3d42.88013832501622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389ec8193a365f7f%3A0xd0f03b4694502966!2sTucano%20Coffee%20Sri%20Lanka!5e0!3m2!1sru!2skg!4v1690540342712!5m2!1sru!2skg" width="650" height="500" loading="lazy"  referrerpolicy="no-referrer-when-downgrade"></iframe>
                         <div className="address">8/25 St.Aaly Tokombaeva </div>
                         </div>
